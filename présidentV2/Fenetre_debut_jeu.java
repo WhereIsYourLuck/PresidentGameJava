5950e0e94c308ad0f0db3a9ad3a69e05871cc16c
mindbreaker2 <77120814+mindbreaker2@users.noreply.github.com>
2022-11-24 14:33:36 +0100

    Add files via upload

---
+++ présidentV2/Fenetre_debut_jeu.java
@@ -0,0 +1,199 @@
+package president_GUI;
+
+
+
+import java.awt.BorderLayout;
+
+
+import java.awt.Color;
+import java.awt.Container;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import game.*;
+
+
+public class Fenetre_debut_jeu{
+			Game game;
+
+		/**
+		 * 
+		 */
+
+		public Fenetre_debut_jeu() {
+				//debug affichage
+			//this.game = g;
+				Border blackline = BorderFactory.createLineBorder(Color.BLACK);
+				//image.setBorder(blackline);
+			
+				JFrame frame = new JFrame("President");
+				//make sure it quits when x is clicked
+				frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				frame.setLayout(new BorderLayout());
+				//add image 
+				JPanel panel_image = new JPanel();
+				ImageIcon icone = new ImageIcon("images/b1fv.png");
+				JLabel image = new JLabel(icone);
+				image.setSize(icone.getIconHeight(),icone.getIconHeight());
+				frame.add(image, BorderLayout.NORTH);
+				
+				
+				//add button distribute  
+			    JPanel panel = new JPanel();
+				JButton button_distribute = new JButton("distribuer");
+				button_distribute.setSize(100,100);
+				panel.add(button_distribute, BorderLayout.NORTH);
+				
+				JButton button_import = new JButton("importer une sauvegarde");
+				button_import.setSize(100,100);
+				panel.add(button_import, BorderLayout.NORTH);
+				
+				frame.add(panel, BorderLayout.CENTER);
+				frame.pack();
+				frame.setVisible(true);
+				//set default size of window
+				frame.setSize(1200, 550);
+				frame.setVisible(true);
+				
+				// regles du jeu 
+				
+				JLabel regles = new JLabel("<html>" + "Règle du jeu :" + "<br>" + "- le joueur qui commence la partie est celui qui a la dame de coeur. (Dame de coeur à toi l'honneur)" + "<br>" + "- Pour poser une carte il faut que sa valeur soit supérieure à celle déjà en jeu." + "<br>" + "- Le 2 est la carte la plus puissante du jeu, elle est considérée comme supérieur à un as et permet de fermer le jeu actuel. Le joueur ayant posé un 2 devra donc rejouer la carte de son choix." + "<br>" + "- Lorsqu'un joueur finit son paquet de cartes il devient président, les autres joueurs poursuivent leurs parties pour déterminer la suite du classement." + "<br>" + "<br>" + "Le président donne ses deux pires cartes au trou du cul." + "<br>" + "Le vice-président donne sa pire carte au vice trou du cul." + "<br>" + "Le vice trou du cul donne sa meilleure carte au vice-président." + "<br>" + "Le trou du cul donne ses deux meilleures cartes au président." + "<br>" + "<br>" + "Le trou du cul commence la partie." +"</html>");
+				panel.add(regles, BorderLayout.SOUTH);
+				
+				
+				//Action listener for distribute
+				button_distribute.addActionListener(new ActionListener() {
+					@Override 
+					public void actionPerformed(ActionEvent e) {
+						clear_JFrame(frame);
+						Game partie = null;
+						try {
+							partie = new Game();
+						} catch (Exception ex) {
+							ex.printStackTrace();
+						}
+						WindowGame jeu = new WindowGame(partie);
+						jeu.affiche_jeux(frame);
+						frame.setMinimumSize(new Dimension(1200,400));
+						
+						//mettre_fondMoche(frame);
+						}
+				});
+				
+				button_import.addActionListener(new ActionListener() {
+					@Override 
+					public void actionPerformed(ActionEvent e) {
+					    //distribution des cartes
+						 try {
+						      File myObj = new File("save.txt");
+						      Scanner myReader = new Scanner(myObj);
+						      JOptionPane affiche_save = new JOptionPane();
+						      String data = "";
+						      while (myReader.hasNextLine()) {
+						    	  //lit la save ligne par ligne
+						         data = data += myReader.nextLine() + "\n";
+						         
+						      }
+						      affiche_save.showMessageDialog(null,"importer depuis la sauvegarde : " + data);
+						      myReader.close();
+						      Scanner assign_value = new Scanner(data);
+						      //sauter ligne vide 
+						      assign_value.nextLine();
+						      
+						      String P1_role = assign_value.nextLine();
+						      int P1_position = assign_value.nextInt();
+						      assign_value.nextLine();
+						      int P1_position_victoire = assign_value.nextInt();
+						      assign_value.nextLine();
+						      System.out.println(P1_role);
+						      System.out.println(P1_position);
+						      System.out.println(P1_position_victoire);
+						      
+						    //sauter ligne vide 
+						      assign_value.nextLine();
+						      String P2_role = assign_value.nextLine();
+						      int P2_position = assign_value.nextInt();
+						      assign_value.nextLine();
+						      int P2_position_victoire = assign_value.nextInt();
+						      assign_value.nextLine();
+						      System.out.println(P2_role);
+						      System.out.println(P2_position);
+						      System.out.println(P2_position_victoire);
+						      
+						    //sauter ligne vide 
+						      assign_value.nextLine();
+						      String P3_role = assign_value.nextLine();
+						      int P3_position = assign_value.nextInt();
+						      assign_value.nextLine();
+						      int P3_position_victoire = assign_value.nextInt();
+						      assign_value.nextLine();
+						      System.out.println(P3_role);
+						      System.out.println(P3_position);
+						      System.out.println(P3_position_victoire);
+						      
+						    //sauter ligne vide 
+						      assign_value.nextLine();
+						      String P4_role = assign_value.nextLine();
+						      int P4_position = assign_value.nextInt();
+						      assign_value.nextLine();
+						      int P4_position_victoire = assign_value.nextInt();
+						      assign_value.nextLine();
+						      System.out.println(P4_role);
+						      System.out.println(P4_position);
+						      System.out.println(P4_position_victoire);
+						      
+						     Player P1 = new Joueur(P1_role, P1_position_victoire,P1_position);
+						     Player P2 = new Bot(P2_role, P2_position_victoire,P2_position);
+						     Player P3 = new Bot(P3_role, P3_position_victoire,P3_position);
+						     Player P4 = new Bot(P4_role, P4_position_victoire,P4_position);
+						     Game partie_saved = new Game(P1,P2,P3,P4);
+						     clear_JFrame(frame);
+								//make sure it quits when x is clicked
+								WindowGame jeu = new WindowGame(partie_saved);
+								jeu.affiche_jeux(frame);
+								frame.setMinimumSize(new Dimension(1000,800));
+						      
+						    } catch (FileNotFoundException e2) {
+						      System.out.println("An error occurred.");
+						      e2.printStackTrace();
+						    } catch (Exception exception) {
+							 exception.printStackTrace();
+						 }
+					}
+				});
+				
+				
+			
+		}
+		
+		
+		
+		public static void clear_JFrame(JFrame fenetre) {
+			fenetre.getContentPane().removeAll();
+			fenetre.repaint();
+		}
+		/*
+		public static void mettre_fondMoche(JFrame fenetre) {
+			fenetre.setContentPane(new JLabel(new ImageIcon("images/Tapis.jpg")));
+		}
+		*/
+
+	}
---
+++ présidentV2/Fenetre_fin_jeu.java
@@ -0,0 +1,205 @@
+package president_GUI;
+
+
+
+import java.awt.BorderLayout;
+
+
+import java.awt.Color;
+import java.awt.Container;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import game.Card;
+import game.*;
+
+
+public class Fenetre_fin_jeu{
+			Game game;
+
+		/**
+		 * 
+		 */
+
+		public Fenetre_fin_jeu(Game g) {
+				//debug affichage
+			this.game = g;
+				Border blackline = BorderFactory.createLineBorder(Color.BLACK);
+				//image.setBorder(blackline);
+			
+				JFrame frame = new JFrame("Fin de la partie");
+				//make sure it quits when x is clicked
+				frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				frame.setLayout(new BorderLayout());
+				
+				//add image 
+				JPanel panel_image = new JPanel();
+				ImageIcon icone = new ImageIcon("images/b1fv.png");
+				JLabel image = new JLabel(icone);
+				image.setSize(icone.getIconHeight(),icone.getIconHeight());
+				frame.add(image, BorderLayout.NORTH);
+				
+				
+				//add button distribute  
+			    JPanel panel = new JPanel();
+			    
+			    
+			    JLabel label = new JLabel("<html>"+"Joueur 1 : " + game.getP1().getNom() + "<br>" + "Joueur 2 : " + game.getP2().getNom() + "<br>" + "Joueur 3 : " + game.getP3().getNom() + "<br>" + "Joueur 4 : " + game.getP4().getNom() + "</html>" );
+				panel.add(label, BorderLayout.SOUTH);
+				
+				JButton button_quit = new JButton("quitter");
+				button_quit.setSize(100,100);
+				panel.add(button_quit, BorderLayout.NORTH);
+				
+				JButton button_reload = new JButton("relancer une partie");
+				button_reload.setSize(100,100);
+				panel.add(button_reload, BorderLayout.NORTH);
+				
+				JButton button_save = new JButton("sauvegarder");
+				button_quit.setSize(100,100);
+				panel.add(button_save, BorderLayout.NORTH);
+				
+				frame.add(panel, BorderLayout.CENTER);
+				frame.pack();
+				frame.setVisible(true);
+				//set default size of window
+				frame.setSize(700, 550);
+				frame.setVisible(true);
+				
+				//Action listener for distribute
+				button_quit.addActionListener(new ActionListener() {
+					@Override 
+					public void actionPerformed(ActionEvent e) {
+						System.exit(0);
+						}
+				});
+				
+				button_save.addActionListener(new ActionListener() {
+					@Override 
+					public void actionPerformed(ActionEvent e) {
+						try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+					              new FileOutputStream("save.txt"), "utf-8"))) {
+							//chose a écrire
+							//ligne vide pour faciliter lecture
+							 writer.write(System.getProperty( "line.separator" ));
+							//P1
+					   writer.write(game.p1.getNom());
+					   writer.write(System.getProperty( "line.separator" ));
+					   writer.write(String.valueOf(game.p1.getPosition()));
+					   writer.write(System.getProperty( "line.separator" ));
+					   writer.write(String.valueOf(game.p1.getPositionVictoire()));
+					   writer.write(System.getProperty( "line.separator" ));
+					   writer.write(System.getProperty( "line.separator" ));
+					   
+					   //P2
+					   writer.write(game.p2.getNom());
+					   writer.write(System.getProperty( "line.separator" ));
+					   writer.write(String.valueOf(game.p2.getPosition()));
+					   writer.write(System.getProperty( "line.separator" ));
+					   writer.write(String.valueOf(game.p2.getPositionVictoire()));
+					   writer.write(System.getProperty( "line.separator" ));
+					   writer.write(System.getProperty( "line.separator" ));
+					   
+					   //P3
+					   writer.write(game.p3.getNom());
+					   writer.write(System.getProperty( "line.separator" ));
+					   writer.write(String.valueOf(game.p3.getPosition()));
+					   writer.write(System.getProperty( "line.separator" ));
+					   writer.write(String.valueOf(game.p3.getPositionVictoire()));
+					   writer.write(System.getProperty( "line.separator" ));
+					   writer.write(System.getProperty( "line.separator" ));
+					   
+					   //P4
+					   writer.write(game.p4.getNom());
+					   writer.write(System.getProperty( "line.separator" ));
+					   writer.write(String.valueOf(game.p4.getPosition()));
+					   writer.write(System.getProperty( "line.separator" ));
+					   writer.write(String.valueOf(game.p4.getPositionVictoire()));
+					   writer.write(System.getProperty( "line.separator" ));
+					   writer.write(System.getProperty( "line.separator" ));
+					   
+					   JOptionPane.showMessageDialog(null,"L'état de la partie a été sauvegardée");
+					   
+					   ///////////////////
+					} catch (UnsupportedEncodingException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					   
+						}
+				});
+
+			button_reload.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					try {
+						game.relancerPartie();
+					} catch (Exception ex) {
+						ex.printStackTrace();
+					}
+					clear_JFrame(frame);
+					//make sure it quits when x is clicked
+					WindowGame jeu = new WindowGame(game);
+					jeu.affiche_jeux(frame);
+					frame.setMinimumSize(new Dimension(1000,800));
+				}
+			});
+				
+				
+				
+				
+			
+		}
+		
+		
+		
+		public static void clear_JFrame(JFrame fenetre) {
+			fenetre.getContentPane().removeAll();
+			fenetre.repaint();
+		}
+		/*
+		public static void mettre_fondMoche(JFrame fenetre) {
+			fenetre.setContentPane(new JLabel(new ImageIcon("images/Tapis.jpg")));
+		}
+		*/
+		public static void setImage_toCard(Player jouer) {
+		
+		}
+		
+		
+		
+		public void affiche_jeux(JFrame fenetre) {
+			//debug affichage
+			Border blackline = BorderFactory.createLineBorder(Color.BLACK);
+			
+			
+		}
+
+	}
